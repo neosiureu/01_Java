f2a5d849b00cc071f4835facd63605e07f6faef3
neosiureu <neosiureu@gmail.com>
2025-02-12 12:00:33 +0900

    최신수업

--- 01_variable/src/edu/kh/variable/ex2/PrintExample.java
+++ 01_variable/src/edu/kh/variable/ex2/PrintExample.java
@@ -20,15 +20,39 @@ public class PrintExample {
 		int inum1=10;
 		int inum2=5;
 		System.out.println(inum1 + " + "+ inum2 +" = " +(inum1+inum2)); // 원할한 표시가 힘들어서
-		System.out.printf("%d + %d = %d",inum1,inum2,inum1+inum2);  // 길어지면 printf는 필수!
-		
+		System.out.printf("%d + %d = %d\n",inum1,inum2,inum1+inum2);  // 길어지면 printf는 필수!
+		System.out.println("탈출문자열 확인차원");
+
 		//System.out.printf("%알파벳, 그 알파벳 자리에 들어갈 값이나 변수를 개수에 맞춰 콤마를 기준으로 나열");
-
-
-
 		
-
+		System.out.printf("%d+%d*%d/2 = %d",inum1,inum1,inum2,(inum1+inum1*inum2/2) );
+		System.out.println();
+		// 패턴 연습
+		int inum3=3;
+		System.out.printf("%d\n",inum3);
+		System.out.printf("%5d\n",inum3); // 같은 숫자를 몇 칸 공간 확보 후 오른쪽 정렬하여 출력하는 방식
+		System.out.printf("%-5d\n",inum3); // 같은 숫자를 몇 칸 공간 확보 후 왼쪽 정렬하여 출력하는 방식
+		System.out.printf("%f\n", 10/4.0); //반올림 처리와 같은 소수점의 자리를 제어하는 방법
+		// %f는 실수를 나타낼 수 있으나 소수점 6자리까지가 디폴트
+		System.out.printf("%.2f\n", 10/4.0); //표시되는 소수점 자리를 바꾸기 위한 방식
 		
+		// 문자와 문자열, 그리고 불리언
+		
+		boolean isTrue = false;
+		char ch = '가';
+		String str = "안녕하세요";
+		System.out.printf("%b / %c / %s \n",isTrue,ch,str);
+		
+		
+		
+		
+		
+		System.out.println("또 다른 탈출 문자열 \\ 을 치면 \\가 하나만 나옴!! \\O/");
+		System.out.println("a\tb\tc\td와 같은 경우 5칸이 하나를 차지함");
+		// 유니코드에 대한 탈출 문자열? 16진수로 네 자리 표시 -> 65를 hexa로 표시한 결과가 41이라서 0041
+		System.out.println("\u0041");
+
+
 	}
 
 }
---
+++ 01_variable/src/edu/kh/variable/ex2/ScannerExample.java
@@ -0,0 +1,20 @@
+package edu.kh.variable.ex2;
+
+import java.util.Scanner; //  Scanner라는 이름의 클래스를 ex2패키지의 이 파일로 수입
+// 비유하자면 class는 설계도이므로 설계도를 이 파일로 가져오는 셈. 
+//설계도만 있으니 기계 자체는 여기에서 준비해서 만들어야 함
+
+
+// import는 다른 패키지에 존재한느 클래스를 들여오는 구문
+
+public class ScannerExample {
+
+	public static void main(String[] args) {
+		// 프로그램 실행 중에 키보드 입력을 받을 수 있는 역할을 하는 클래스로부터 객체 생성. 즉 설계도로부터 기계 만들기
+		Scanner sc = new Scanner(System.in);
+		// Scanner라는 설계도를 이용해서 만든 기계의 이름 sc. 매개변수는 키보드 입력이므로 System.in
+		//sc라는 객체는 만들었지만 
+
+	}
+
+}
